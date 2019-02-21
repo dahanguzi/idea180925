d1f9f24ab23236d5fe76a7b8b5d2ba2b55936271
dahanguzi <1160378964@qq.com>
2019-02-21 18:45:47 +0800

    向下减

--- juc/src/main/java/com/atguigu/juc/threadrelevantclass/TestCountDownLatch.java
+++ juc/src/main/java/com/atguigu/juc/threadrelevantclass/TestCountDownLatch.java
@@ -1,4 +1,23 @@
 package com.atguigu.juc.threadrelevantclass;
 
+
+import lombok.Getter;
+
+import java.util.concurrent.CountDownLatch;
+
 public class TestCountDownLatch {
+    public static void main(String[] args) throws Exception {
+        CountDownLatch ountDownLatch = new CountDownLatch(6);
+
+        for (int i = 1; i <= 6; i++) {
+            //final int temp = i;
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName()+"\t国被灭");
+                ountDownLatch.countDown();
+            },WarringStates.foreach_Enum(i).getRetMessage()).start();
+        }
+        ountDownLatch.await();
+        System.out.println(Thread.currentThread().getName()+"**********一统华夏");
+    }
 }
+
